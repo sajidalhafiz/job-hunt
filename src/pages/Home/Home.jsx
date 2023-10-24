a325be1a3ff3618a0893c458efd19f83b128015d
tryit-100 <safwan.std21@gmail.com>
2023-10-24 22:28:44 +0600

    project initiated with react-router, tailwind, daisyUi and given assets

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className='text-3xl font-bold'>home</h2>
+            <button className="btn btn-primary">Primary</button>
+        </div>
+    );
+};
+
+export default Home;
